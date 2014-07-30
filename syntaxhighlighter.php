63b725252a8264bc5321f0c71b838cfed4afdef8
Brice Boucard <brice.boucard@gmail.com>
2014-07-30 18:41:31 +0200

    Using after_render function rather than before_render : we don't need to add twig vars to the themes.

--- syntaxhighlighter.php
+++ syntaxhighlighter.php
@@ -7,10 +7,7 @@
  * to Pico
  *
  * @author Brice Boucard
- *
- *
- *
- * @link https://github.com/DanReeves/picotags/
+ * @link https://github.com/bricebou/pico_SyntaxHighlighter
  * @license http://bricebou.mit-license.org/
  */
 
@@ -23,58 +20,55 @@ class SyntaxHighlighter {
         {
             $this->synhigh_theme = $settings['synhigh']['theme'];
         }
-        if (isset($settings['synhigh']['autoloader']))
+
+        if (isset($settings['synhigh']['autoloader']) && $settings['synhigh']['autoloader'] === true)
         {
-        	if ($settings['synhigh']['autoloader'] === true)
-        	{
-        		$this->synhigh_loader = '<script src="'.$this->theme_url.'/scripts/syntaxhighlighter/scripts/shAutoloader.js"></script>';
-        		if (isset($settings['synhigh']['autobrush']) && is_array($settings['synhigh']['autobrush']))
-        		{
-        			$synhigh_autobrushscripts = '';
-        			$this->synhigh_autobrush = $settings['synhigh']['autobrush'];
-        			foreach ($this->synhigh_autobrush as $key => $value)
-		            {
-		            	$synhigh_autobrushscripts .= '\''.$key.'		'.$this->theme_url.'/scripts/syntaxhighlighter/scripts/shBrush'.$value.'.js\','.PHP_EOL;
-		            }
-		            $this->synhigh_autobrushscripts = substr($synhigh_autobrushscripts,0,-3);
-        		}
-        	}
-        	else
-        	{
-        		if (isset($settings['synhigh']['brush']) && is_array($settings['synhigh']['brush']))
-		        {
-		        	$synhigh_brushscripts = '';
-		            $this->synhigh_brush = $settings['synhigh']['brush'];
-		            foreach ($this->synhigh_brush as $key => $value)
-		            {
-		            	$synhigh_brushscripts .= '<script src="'.$this->theme_url.'/scripts/syntaxhighlighter/scripts/shBrush'.$value.'.js"></script>';
-		            }
-		            $this->synhigh_loader = $synhigh_brushscripts.PHP_EOL.'<script>SyntaxHighlighter.all()</script>';
-		        }
-        	}
+    		$this->synhigh_loader = '<script src="'.$this->theme_url.'/scripts/syntaxhighlighter/scripts/shAutoloader.js"></script>';
+    		if (isset($settings['synhigh']['autobrush']) && is_array($settings['synhigh']['autobrush']))
+    		{
+    			$synhigh_autobrushscripts = '';
+    			$this->synhigh_autobrush = $settings['synhigh']['autobrush'];
+    			foreach ($this->synhigh_autobrush as $key => $value)
+	            {
+	            	$synhigh_autobrushscripts .= '\''.$key.'		'.$this->theme_url.'/scripts/syntaxhighlighter/scripts/shBrush'.$value.'.js\','.PHP_EOL;
+	            }
+	            $this->synhigh_autobrushscripts = substr($synhigh_autobrushscripts,0,-3);
+    		}
         }
-        
+    	elseif((isset($settings['synhigh']['autoloader']) && $settings['synhigh']['autoloader'] === false) || empty($settings['synhigh']['autoloader']))
+        {
+        	if (isset($settings['synhigh']['brush']) && is_array($settings['synhigh']['brush']))
+	        {
+	        	$synhigh_brushscripts = '';
+	            $this->synhigh_brush = $settings['synhigh']['brush'];
+	            foreach ($this->synhigh_brush as $key => $value)
+	            {
+	            	$synhigh_brushscripts .= '<script src="'.$this->theme_url.'/scripts/syntaxhighlighter/scripts/shBrush'.$value.'.js"></script>';
+	            }
+	            $this->synhigh_loader = $synhigh_brushscripts.PHP_EOL.'<script>SyntaxHighlighter.all()</script>';
+	        }
+        }  
 	}
 
-
-	public function before_render(&$twig_vars, &$twig)
+	public function after_render(&$output)
 	{
-		
-		$twig_vars['synhigh_head'] = '<link href="'.$this->theme_url.'/scripts/syntaxhighlighter/styles/shCore.css" rel="stylesheet" type="text/css" />
-	<link href="'.$this->theme_url.'/scripts/syntaxhighlighter/styles/shTheme'.$this->synhigh_theme.'.css" rel="stylesheet" type="text/css" />
-	<script src="'.$this->theme_url.'/scripts/syntaxhighlighter/scripts/shCore.js"></script>'.
-	$this->synhigh_loader;
-
-		if (isset($settings['synhigh']['autoloader']) && $settings['synhigh']['autoloader'] === true)
-		{
-			$twig_vars['synhigh_autobrushscripts'] = '<script type="text/javascript">
-				SyntaxHighlighter.autoloader('.PHP_EOL.
-				$this->synhigh_autobrushscripts
-				.PHP_EOL.');
-				SyntaxHighlighter.all();
-		    </script>';
+		if (isset($this->synhigh_autobrushscripts) && $this->synhigh_autobrushscripts != '') {
+			$synhigh_body = '<script type="text/javascript">
+					SyntaxHighlighter.autoloader('.PHP_EOL.
+					$this->synhigh_autobrushscripts
+					.PHP_EOL.');
+					SyntaxHighlighter.all();
+		    	</script>';
+		    
+	    	$output = str_replace('</body>', PHP_EOL.$synhigh_body.'</body>', $output);
 		}
-		
+
+		$synhigh_head = '
+	<link href="'.$this->theme_url.'/scripts/syntaxhighlighter/styles/shCore.css" rel="stylesheet" type="text/css" />
+	<link href="'.$this->theme_url.'/scripts/syntaxhighlighter/styles/shTheme'.$this->synhigh_theme.'.css" rel="stylesheet" type="text/css" />
+	<script src="'.$this->theme_url.'/scripts/syntaxhighlighter/scripts/shCore.js"></script>'.PHP_EOL.'	'.$this->synhigh_loader;
+
+		$output = str_replace('</head>', PHP_EOL.$synhigh_head.'</head>', $output);
 	}
 }
 
